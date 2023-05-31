38c45d9729816ef8488392688805279779ea16e8
B100983 <sigbjorn.berntzen@nav.no>
2023-05-31 11:25:58 +0200

    start

---
+++ src/main/java/no/nav/emottak/emottakcamelpoc/EmottakCamelPocApplication.java
@@ -0,0 +1,13 @@
+package no.nav.emottak.emottakcamelpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmottakCamelPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmottakCamelPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/no/nav/emottak/emottakcamelpoc/EmottakCamelPocApplicationTests.java
@@ -0,0 +1,13 @@
+package no.nav.emottak.emottakcamelpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmottakCamelPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
